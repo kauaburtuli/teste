f5d344c0928b2b8f32ea6539ef454e16d06d850a
kauaburtuli <kaua.burtuli@escola.pr.gov.br>
2026-07-05 20:43:37 -0300

    2

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-const cards = document.querySelectorAll(".card");
+const cards=document.querySelectorAll(".card");
 
-window.addEventListener("scroll", () => {
+window.addEventListener("scroll",()=>{
 
-    cards.forEach(card => {
+cards.forEach(card=>{
 
-        const pos = card.getBoundingClientRect().top;
+const pos=card.getBoundingClientRect().top;
 
-        if (pos < window.innerHeight - 100) {
+if(pos<window.innerHeight-100){
 
-            card.style.opacity = "1";
-            card.style.transform = "translateY(0)";
+card.style.opacity="1";
+card.style.transform="translateY(0)";
 
-        }
-
-    });
+}
 
 });
 
-cards.forEach(card => {
+});
 
-    card.style.opacity = "0";
-    card.style.transform = "translateY(40px)";
-    card.style.transition = ".8s";
+cards.forEach(card=>{
+
+card.style.opacity="0";
+card.style.transform="translateY(40px)";
+card.style.transition=".8s";
 
 });
